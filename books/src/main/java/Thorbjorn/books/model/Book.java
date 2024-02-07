ff47993362b02e183a004663fafbf30a1509f44a
Thorbjørn Melheim <thomelheim@gmail.com>
2024-02-07 16:27:38 +0100

    Added fields for book model

--- books/src/main/java/Thorbjorn/books/model/Book.java
+++ books/src/main/java/Thorbjorn/books/model/Book.java
@@ -1,4 +1,26 @@
 package Thorbjorn.books.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "books")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
+    @Id
+    private ObjectId id;
+    private String isbn13;
+    private String isbn10;
+    private String title;
+    private String subtitle;
+    private String authors;
+    private String categories;
+    private String thumbnail;
+    private String description;
+    private int published_year;
 }
